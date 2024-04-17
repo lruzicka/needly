914cbf40e2e24d5bb270ff8a0e82ab18b1d48cd5
Lukas Ruzicka <lruzicka@redhat.com>
2024-04-17 10:47:32 +0200

    Update copyright info.

--- needly/needly/needly.py
+++ needly/needly/needly.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 18 08:46:48 2018
-
-@author: Lukáš Růžička (lruzicka@redhat.com)
-"""
+#
+# Copyright 2018, Red Hat, Inc
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Authors:
+#   Lukáš Růžička <lruzicka@redhat.com>
 
 import tkinter as tk
 import io
